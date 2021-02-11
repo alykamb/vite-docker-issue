b317ca6ad9911a3ff91399e842fff55342ac0eb6
Alykam Budzaki <alykam@meucrediario.com.br>
2021-02-11 13:49:40 -0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {join} from 'path'
- // https://vitejs.dev/config/
+import { join } from 'path'
+import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),],
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: join(__dirname, '/src/main.ts')
-    }
-  }
+    plugins: [vue()],
+    build: {
+        // generate manifest.json in outDir
+        manifest: true,
+        rollupOptions: {
+            // overwrite default .html entry
+            input: join(__dirname, '/src/main.ts'),
+        },
+    },
 })
